9117ce6d8afe912fe8eccc1dc6912c73f637e99f
Valery <102878394+Arturovicz@users.noreply.github.com>
2023-05-08 10:54:51 +0200

    Update 01_new_horizons.cpp

--- 01_new_horizons.cpp
+++ 01_new_horizons.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "This world is mine" << '\n';
+    std::cout << "______________________" << '\n';
     std::cout << "I will establish peace" << '\n';
     return 0;
 }
